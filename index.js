24965f1f1c6af67a0fbf5ab6cd6a48265d499d4b
Andres A <acosta.andres.r@gmail.com>
2020-09-08 13:54:12 -0500

    Created README format usign template literals. Also, added question to be prompted.

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is title of project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of the project?"
+  },
+  {
+    type: "confirm",
+    name: "table", // true or false
+    message: "Does the README require a table of content?"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage information?"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your application",
+    choices:[
+      'MIT',
+      'GPLv3',
+      'AGPL',
+      'ISC',
+      'IPL 1.0'
+    ]
+  },
+  {
+    type: "input",
+    name: "user",
+    message: "Enter your name"
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What are the contribution guidelines?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What are the test instructions?"
+  }
 ];
 
 // function to write README file
@@ -14,3 +76,4 @@ function init() {
 
 // function call to initialize program
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,80 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  // String of Table of content
+  const tableOfContent = `### Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)`
+
+  // LICENSE:
+  const licenses = {
+    MIT: '![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)',
+    GPLv3: '![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)',
+    AGPL: '![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)',
+    ISC: '![ISC License](https://img.shields.io/badge/License-ISC-blue.svg)',
+    'IPL 1.0': '![IPL 1.0 License](https://img.shields.io/badge/License-IPL%201.0-blue.svg)'
+  }
+
+  return `<h1 align="center"> ${ data.title } </h1> 
+
+<div align="center"> 
+
+${ licenses[data.license] }
+</div>
+  
+### Description
+${ data.description }
+
+${(data.table) ? tableOfContent : ''}
+
+### Installation
+${ data.installation }
+
+### Usage
+${ data.usage }
+
+### License
+
+Copyright (c) ${data.user}. All rights reserved.
+Licensed under the [${ data.license } license](LICENSE.md).
+
+### Credits
+
+👤 **${data.user}** (author): [GitHub](https://github.com/${data.github})
+
+### Contributing
+${data.contributing}
+
+### Test
+${data.test}
+### Questions
+
+ For more information, additional questions or comments, contact [${data.user}](https://github.com/${data.github}).
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
+//testing:
+// const data = {
+//   title: "Project Readme",
+//   description: "Here is an placeholder for description.",
+//   table: true,
+//   installation: "This is a placeholder for description.",
+//   usage: "Placeholder for usage.",
+//   license: "MIT",
+//   user: 'Andres Acosta',
+//   github: 'acosta-andres-r',
+//   contributing: 'By participating in this project, you agree to abide by Contributor Convenant guidelines.',
+//   test: '```node index.js```'
+// }
+
+// console.log(generateMarkdown(data));
